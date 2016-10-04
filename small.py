0cd66072314e0b3e42beca9b64240f72243fb1ac
sh999 <sh999gh@gmail.com>
2016-10-04 07:12:59 -0400

    Do one iteration

--- small.py
+++ small.py
@@ -51,7 +51,7 @@ def make_site_list(adj_list):
 				pass
 	return site_list
 def make_init(site_list):
-	i = 1
+	i = 2
 	pr_vec = {}
 	for pg in site_list:
 		pr_vec[pg] = i 
@@ -120,7 +120,66 @@ def scalar_times_matrix(scalar, matrix):
 			# print ""
 		result[k] = to_insert 			# Insert multiple elements (row)
 	return result
-def main():
+def surfer_times_pr(inv_damping,vector):
+	'''
+		Multiplies "Matrix" of random surfer (term 2 of equation)
+		 by pagerank vector
+		But not really storing surfer matrix because it's full
+		Instead, multiply element by element
+	'''
+	# pr_vector = {}	
+	# for row in vector:						# Loop through result vector
+	# 	# print "\npr row:", row
+	# 	curr_sum = 0
+	# 	print "len:" len
+	# 	for i in range(0, len(vector)):
+	# 		print "hey"
+	# 	# if row in matrix:
+	# 	# 	# print "matrix[row]:", matrix[row]
+	# 	# 	for elem in matrix[row]:		# Loop through matrix row	
+	# 	# 		# print "multiplying", elem, ":", matrix[row][elem], "by", vector[row]
+	# 	# 		mult = matrix[row][elem] * vector[row]		# Multiply matrix row by vector column
+	# 	# 		curr_sum += mult 			# Sum terms successively
+	# 		# print "curr_sum:", curr_sum
+	# 	pr_vector[row] = curr_sum
+	# # pprint(pr_vector)
+	# return pr_vector
+	summed = 0
+	for k in vector:
+		summed = summed + vector[k]
+	# print "sum:", summed
+	return summed
+def add_faux_matrices(matrix, faux_matrix):
+	'''
+		Does the final addition of pagerank iteration of form:
+		matrix + faux_matrix
+		where faux_matrix is the pagerank vector after multiplied by surfer matrix
+	'''
+	result = {}
+	for k, v in matrix.iteritems():
+		to_insert = {}
+		for i in v:
+			to_insert[i] = faux_matrix + matrix[k][i] 	# Multiply same scalar by each element for all elements in row
+			# print "\tto_insert\t",
+			# pprint(to_insert)
+			to_insert.update(to_insert) 	# Putative row to insert
+			# print ""
+		result[k] = to_insert 			# Insert multiple elements (row)
+	return result
+def add_vectors(vector1, vector2):
+	'''
+		After distributing the pr vectors, add them together
+		vector2 is not really a vector; it's a number but represents vector
+		 filled with the same number
+	'''
+	result = {}
+	for k in vector1:
+		# print "k:", k
+		# print "vector1[k]:", vector1[k]
+		result[k] = vector1[k] + vector2
+	return result
+
+def one_iteration():
 	damping = 0.85  			# Damping factor
 	inv_damping = 1 - damping
 	# adj_list = {"A":{"B":0,"C":0}, "B":{"C":0},"C":{"A":0},"D":{"C":0}}
@@ -131,7 +190,7 @@ def main():
 	rotated_weighted_damping = scalar_times_matrix(damping,rotated_weighted)		 # Multiply weighted matrix by damping factor (alpha * S)
 	sites_list = make_site_list(adj_list)				 # Each unique site has an integer ID
 	pr_vec = make_init(sites_list)					 # Make initial PR vector, which is a vector with unique sites w/ score 1
-	result = matrix_times_vector(rotated_weighted, pr_vec)
+	rw_v = matrix_times_vector(rotated_weighted, pr_vec)
 	print "\nInitial adj_list"
 	pprint(adj_list)
 	print "\nunweighted list"
@@ -145,9 +204,16 @@ def main():
 	print "\npr_vec init"
 	pprint(pr_vec)
 	print "\nrotated_weighted times pr_vec"
-	pprint(result)
+	pprint(rw_v)
 	print "\nrotated weighted times damping"
 	pprint(rotated_weighted_damping)
-
-
-main()
+	print "\nterm1:"
+	term1 = matrix_times_vector(rotated_weighted_damping, pr_vec)
+	pprint(term1)
+	print "\nterm2:"
+	term2 = surfer_times_pr(inv_damping,pr_vec)
+	pprint(term2)
+	print "\nadded together:"
+	added = add_vectors(term1,term2)
+	pprint(added)
+one_iteration()
