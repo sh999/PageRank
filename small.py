c978830d405a11e3558c88ba25645ece37a9b72d
sh999 <sh999gh@gmail.com>
2016-10-04 06:31:40 -0400

    Multiply scalar by vector

--- small.py
+++ small.py
@@ -21,7 +21,6 @@ def calc_unweighted(adj_list):
 def calc_weighted(adj_list):
 	'''
 		Return adj list based on # of sites (vote dilution); if link to 3 sites, each element is 1/3
-		However, this is in inverse, so 1/3 becomes 3
 	'''
 	weighted = copy.deepcopy(adj_list)
 	for k, v in weighted.iteritems():
@@ -29,7 +28,7 @@ def calc_weighted(adj_list):
 		# print "# links:", len(v)
 		# print "links:", v
 		for name in v:
-			v[name] = len(v)
+			v[name] = 1.0/len(v)
 		# print ""
 	# pprint(weighted)
 	return weighted
@@ -103,29 +102,52 @@ def rotate(matrix):
 				rotated[i] = {}
 			rotated[i].update(to_insert) 
 	return rotated
+def scalar_times_matrix(scalar, matrix):
+	'''
+		Multiply scalar times matrix.
+		Important for, say, alpha times S 
+	'''
+	result = {}
+	pprint(matrix)
+	for k, v in matrix.iteritems(): 	# Loop through matrix row elements
+		# print "k:", k
+		to_insert = {} 					# Temporary row element to insert
+		for i in v: 					# Loop through columns
+			to_insert[i] = scalar * matrix[k][i] 	# Multiply same scalar by each element for all elements in row
+			# print "\tto_insert\t",
+			# pprint(to_insert)
+			to_insert.update(to_insert) 	# Putative row to insert
+			# print ""
+		result[k] = to_insert 			# Insert multiple elements (row)
+	return result
 def main():
-	damping = 0.85
+	damping = 0.85  			# Damping factor
+	inv_damping = 1 - damping
 	# adj_list = {"A":{"B":0,"C":0}, "B":{"C":0},"C":{"A":0},"D":{"C":0}}
 	adj_list = {"H":{"Ab":0,"P":0,"L":0}, "Ab":{"H":0},"P":{"H":0},"L":{"H":0,"A":0,"B":0,"C":0,"D":0,}}
 	unweighted = calc_unweighted(adj_list)	 # Adj list where 1 = link present
 	weighted = calc_weighted(adj_list)  	 # Get adj list of raw numbers (1 = outlinks to)
 	rotated_weighted = rotate(weighted)		 # Rotate weighted adj list to proper form
+	rotated_weighted_damping = scalar_times_matrix(damping,rotated_weighted)		 # Multiply weighted matrix by damping factor (alpha * S)
 	sites_list = make_site_list(adj_list)				 # Each unique site has an integer ID
 	pr_vec = make_init(sites_list)					 # Make initial PR vector, which is a vector with unique sites w/ score 1
 	result = matrix_times_vector(rotated_weighted, pr_vec)
-	print "Initial adj_list"
+	print "\nInitial adj_list"
 	pprint(adj_list)
-	print "unweighted list"
+	print "\nunweighted list"
 	pprint(unweighted)
-	print "weighted list"
+	print "\nweighted list"
 	pprint(weighted)
-	print "rotated_weighted"
+	print "\nrotated_weighted"
 	pprint(rotated_weighted)
-	print "sites_list"
+	print "\nsites_list"
 	pprint(sites_list)
-	print "pr_vec init"
+	print "\npr_vec init"
 	pprint(pr_vec)
-	print "rotated_weighted times pr_vec"
+	print "\nrotated_weighted times pr_vec"
 	pprint(result)
+	print "\nrotated weighted times damping"
+	pprint(rotated_weighted_damping)
+
 
 main()
